e4261fd9b43f3d22a936b3ce8c84463469b09f6e
Coder-PadmeshGN <89740697+Coder-PadmeshGN@users.noreply.github.com>
2022-02-27 13:16:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def upload():
         KEY = "aec15ff556c449488f778af556ac567c"
         ENDPOINT = "https://facialemotion1.cognitiveservices.azure.com//face/v1.0/detect"
         face_client = FaceClient(ENDPOINT, CognitiveServicesCredentials(KEY))
+        upload_image.save(upload_image.filename)
         image_stream=open(upload_image.filename,"rb")
         #single_image_name = os.path.basename(upload_image)
         headers = {'Content-Type': 'application/octet-stream','Ocp-Apim-Subscription-Key': KEY}
@@ -24,23 +25,26 @@ def upload():
         response= requests.post(ENDPOINT, params=params, headers=headers, data=image_stream)
         response.raise_for_status()
         faces = response.json()
-        image_orig = open(upload_image.filename, "rb").read()
-        img = Image.open(BytesIO(image_orig))
-        draw = ImageDraw.Draw(img)
-        for face in faces:
-            fa = face["faceAttributes"]
-            emotion = fa["emotion"]
-            values = list(emotion.values())
-            keys = list(emotion.keys())
-            maximum = max(values)
-            result = "Emotion: " + keys[values.index(maximum)]
-            draw.rectangle(getRectangle(face), outline='red')
-            font = ImageFont.truetype("arial.ttf", 30, )
-            draw.text((getRectangle(face)[0], getRectangle(face)[1]), result, "white", font=font, weight="bold")
-            img.show()
-        return render_template("Upload.html")
+        drawFaceRectangles(upload_image,faces)
+    return render_template("Upload.html")
 
-#@app.route("/Upload")
+def drawFaceRectangles(upload_image,faces):
+    image_orig = open(upload_image.filename, "rb").read()
+    img = Image.open(BytesIO(image_orig))
+    draw = ImageDraw.Draw(img)
+    for face in faces:
+        fa = face["faceAttributes"]
+        emotion = fa["emotion"]
+        values = list(emotion.values())
+        keys = list(emotion.keys())
+        maximum = max(values)
+        result = "Emotion: " + keys[values.index(maximum)]
+        draw.rectangle(getRectangle(face), outline='red')
+        font = ImageFont.truetype("arial.ttf", 30, )
+        draw.text((getRectangle(face)[0], getRectangle(face)[1]), result, "white", font=font, weight="bold")
+        img.show()
+
+
 def getRectangle(faceDictionary):
     rect = faceDictionary["faceRectangle"]
     left = rect["left"]
@@ -48,7 +52,7 @@ def getRectangle(faceDictionary):
     right = left + rect["width"]
     bottom = top + rect["height"]
     return ((left), (top),(right),(bottom))
-    #return render_template("Upload.html")
+
 
 
 if __name__ == '__main__':
